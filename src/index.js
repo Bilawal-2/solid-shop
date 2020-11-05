4821e4402274ab75ac585ce4b22dd20fb87a49ea
Muhammad Bilawal <awal@Muhammads-MacBook-Pro.local>
2020-11-05 18:00:10 +0500

    This is a starting point of our e-shop

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+let Hello = ()=>{
+  return (<h1>Hello B-Glasses</h1>)
+}
+
+ReactDOM.render(
+
+    <Hello />
+  ,
+  document.getElementById('root')
+);
+
